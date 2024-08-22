d7b58e7b3421a21e13ecc890525ab4d253b573d1
mietl <mietl@users.noreply.github.com>
2024-08-22 01:36:57 +0000

    fix push

--- js/search.js
+++ js/search.js
@@ -1,1 +1,80 @@
-let searchData;function loadData(e){if(searchData)searchkey(e);else{const t=new XMLHttpRequest;t.open("GET","/content.json",!0),t.onload=function(){if(this.status>=200&&this.status<300){const t=JSON.parse(this.response||this.responseText);searchData=t instanceof Array?t:t.posts,searchkey(e)}else console.error(statusText)},t.onerror=function(){console.error(statusText)},t.send()}}function searchkey(e){e.forEach((e=>{searchData.forEach((t=>{const s={},a=new RegExp(e,"g");let n=!1;-1!==t.title.search(a)&&(s.title=t.title.replace(a,`<span class="keyword">${e}</span>`),n=!0);const o=t.text.search(a);-1!==o&&(s.text=`…${t.text.substring(o,o+18)}…`,s.text=s.text.replace(a,`<span class="keyword">${e}</span>`),n=!0),n&&(s.title=s.title?s.title:t.title,s.text=s.text?s.text:t.text,s.href=`/${t.path}`,render(s))}))}))}function render(e){const t=document.createElement("div");t.className="search-result",t.innerHTML=`<a href=${e.href}><h3>${e.title}</h3>\n    <span class="content">${e.text}<br>　</span></a>`,document.getElementsByClassName("search-body")[0].appendChild(t)}const key=decodeURI(location.search.split("?q=")[1]);function sclose(){document.getElementById("nexmoe-search-space").style.display="none"}function sinput(){document.getElementsByClassName("search-body")[0].innerHTML="",loadData(format(document.getElementsByClassName("search-input")[0].value))}function format(e){return e.replace(/[ ]/g,"").split(" ")}void 0!==key&&"undefined"!==key&&(document.getElementsByClassName("search-input")[0].value=key,loadData(format(key)),document.getElementById("nexmoe-search-space").style.display="flex");
+// 参考 https://chaooo.github.io/article/20161109.html
+let searchData;
+function loadData(arg) {
+    if (!searchData) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/content.json', true);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                const res = JSON.parse(this.response || this.responseText);
+                searchData = res instanceof Array ? res : res.posts;
+                searchkey(arg);
+            } else {
+                console.error(statusText);
+            }
+        };
+        xhr.onerror = function() {
+            console.error(statusText);
+        };
+        xhr.send();
+    } else {
+        searchkey(arg);
+    }
+}
+
+function searchkey(keyword) {
+    keyword.forEach(word => {
+        searchData.forEach(post => {
+            const rend = {};
+            const reg = new RegExp(word, 'g');
+            let flag = false;
+            if (post.title.search(reg) !== -1) {
+                rend.title = post.title.replace(reg, `<span class="keyword">${word}</span>`);
+                flag = true;
+            }
+            const textpos = post.text.search(reg);
+            if (textpos !== -1) {
+                rend.text = `…${post.text.substring(textpos, textpos + 18)}…`;
+                rend.text = rend.text.replace(reg, `<span class="keyword">${word}</span>`);
+                flag = true;
+            }
+            if (flag) {
+                rend.title = !rend.title ? post.title : rend.title;
+                rend.text = !rend.text ? post.text : rend.text;
+                rend.href = `/${post.path}`;
+                render(rend);
+            }
+        });
+    });
+}
+
+function render(data) {
+    const ele = document.createElement('div');
+    ele.className = 'search-result';
+    ele.innerHTML = `<a href=${data.href}><h3>${data.title}</h3>
+    <span class="content">${data.text}<br>　</span></a>`;
+    document.getElementsByClassName('search-body')[0].appendChild(ele);
+}
+
+// 主程序
+const key = decodeURI(location.search.split('?q=')[1]);
+if (key !== undefined && key !== 'undefined') {
+    document.getElementsByClassName('search-input')[0].value = key;
+    loadData(format(key));
+    document.getElementById('nexmoe-search-space').style.display = 'flex';
+}
+
+// 事件
+function sclose() {
+    document.getElementById('nexmoe-search-space').style.display = 'none';
+}
+
+function sinput() {
+    document.getElementsByClassName('search-body')[0].innerHTML = '';
+    loadData(format(document.getElementsByClassName('search-input')[0].value));
+}
+
+// 搜索词格式化
+function format(word) {
+    return word.replace(/[ ]/g, '').split(' ');
+}
